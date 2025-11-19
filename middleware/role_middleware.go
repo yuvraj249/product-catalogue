d762b61152d113e6ae6278c0ab4fddb873e4ebac
yuvraj249 <yuvraj@tech-x.com>
2025-11-19 13:38:01 +0530

    commit for updating role_middleware

--- middleware/role_middleware.go
+++ middleware/role_middleware.go
@@ -5,58 +5,17 @@ import (
 	"slices"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/golang-jwt/jwt/v5"
 )
 
 func RoleAllowed(allowedRoles ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		claims1, ok := c.Get("claims")
-		if !ok {
-			token1, exists := c.Get("token")
-			if !exists {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication Required"})
-				c.Abort()
-				return
-			}
-
-			token, ok := token1.(*jwt.Token)
-			if !ok || token == nil {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token!!"})
-				c.Abort()
-				return
-			}
-			claims2, ok2 := token.Claims.(jwt.MapClaims)
-			if !ok2 {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims!!"})
-				c.Abort()
-				return
-			}
-			claims1 = claims2
-
-		}
-
-		claims, ok := claims1.(jwt.MapClaims)
-		if !ok {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims!!"})
-			c.Abort()
-			return
-		}
-
-		roleValue, ok := claims["role"]
-		if !ok {
-			c.JSON(http.StatusForbidden, gin.H{"error": "role missing in token!!"})
-			c.Abort()
-			return
-		}
-		role, ok := roleValue.(string)
-		if !ok || role == "" {
-			c.JSON(http.StatusForbidden, gin.H{"error": "Invalid or empty role in token!!"})
+		role := c.GetString("role")
+		if role == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "role missing (auth required)"})
 			c.Abort()
 			return
 		}
 		if slices.Contains(allowedRoles, role) {
-			c.Set("role", role)
 			c.Next()
 			return
 		}
