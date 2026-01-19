00cf9693cd069c68b9ac6ed37305c915beec4603
yuvraj249 <yuvraj@tech-x.com>
2026-01-19 10:09:29 +0530

    updated cypress test cases for login

--- frontend/cypress/e2e/loginPage.cy.js
+++ frontend/cypress/e2e/loginPage.cy.js
@@ -10,202 +10,85 @@ describe('Login Page - Auth and Validation', () => {
 
   it('loads Login Page UI', () => {
     cy.contains('Sign in to your account').should('be.visible')
-    cy.get('input[placeholder="abc@gmail.com"]').should('be.visible')
-    cy.get('input[placeholder="password"]').should('be.visible')
-    cy.contains('Sign In').should('be.visible')
+    cy.get('[data-cy="email-input"]').should('be.visible')
+    cy.get('[data-cy="password-input"]').should('be.visible')
+    cy.get('[data-cy="login-btn"]').should('be.visible')
   })
 
 
   it('blocks empty email', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
+    cy.get('[data-cy="password-input"]').type('Valid@123')
+    cy.get('[data-cy="login-btn"]').click()
 
-    cy.get('input[placeholder="password"]').type('Valid@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
+    cy.contains('.Toastify__toast', 'Email is required')
   })
-
-
 
   it('blocks invalid email format', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
+    cy.get('[data-cy="email-input"]').type('invalidemail.com')
+    cy.get('[data-cy="password-input"]').type('Valid@123')
+    cy.get('[data-cy="login-btn"]').click()
 
-    cy.get('input[placeholder="abc@gmail.com"]').type('invalidemail.com')
-    cy.get('input[placeholder="password"]').type('Valid@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
+    cy.contains('.Toastify__toast', 'Enter a valid email')
   })
-
 
   it('blocks email starting with @', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
+    cy.get('[data-cy="email-input"]').type('@test.com')
+    cy.get('[data-cy="password-input"]').type('Valid@123')
+    cy.get('[data-cy="login-btn"]').click()
 
-    cy.get('input[placeholder="abc@gmail.com"]').type('@test.com')
-    cy.get('input[placeholder="password"]').type('Valid@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
+    cy.contains('.Toastify__toast', 'Enter a valid email')
   })
-
 
   it('blocks email with consecutive dots', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
+    cy.get('[data-cy="email-input"]').type('test..mail@gmail.com')
+    cy.get('[data-cy="password-input"]').type('Valid@123')
+    cy.get('[data-cy="login-btn"]').click()
 
-    cy.get('input[placeholder="abc@gmail.com"]').type('test..mail@gmail.com')
-    cy.get('input[placeholder="password"]').type('Valid@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
+    cy.contains('.Toastify__toast', 'Login failed')
   })
 
+  it('blocks empty password', () => {
+    cy.get('[data-cy="email-input"]').type('tester@tester.com')
+    cy.get('[data-cy="login-btn"]').click()
 
-  it('blocks small passwords', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
-
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('Valid')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
+    cy.contains('.Toastify__toast', 'Password is required')
   })
-
-
-  it('blocks password without uppercase', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
-
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('valid@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
-  })
-
-
-  it('blocks password without lowercase', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
-
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('VALID@123')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
-  })
-
-
-  it('blocks password without number', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
-
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('VALID@valid')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
-  })
-
-
-  it('blocks password without special character', () => {
-    cy.intercept('POST', '**/auth/login').as('loginRequest')
-
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('Valid12345')
-    cy.contains('Sign In').click()
-
-    cy.wait('@loginRequest')
-      .its('response.statusCode')
-      .should('eq', 400)
-
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Login failed')
-  })
-
 
 
   it('blocks invalid credentials', () => {
     cy.intercept('POST', '**/auth/login').as('loginRequest')
 
-    cy.get('input[placeholder="abc@gmail.com"]').type('tester@tester.com')
-    cy.get('input[placeholder="password"]').type('Valid@12345')
-    cy.contains('Sign In').click()
+    cy.get('[data-cy="email-input"]').type('tester@tester.com')
+    cy.get('[data-cy="password-input"]').type('Wrong@123')
+    cy.get('[data-cy="login-btn"]').click()
 
     cy.wait('@loginRequest')
       .its('response.statusCode')
       .should('eq', 401)
 
-    cy.get('.Toastify__toast')
-      .should('be.visible')
-      .and('contain.text', 'Incorrect email or password')
+    cy.contains('.Toastify__toast', 'Incorrect email or password')
   })
 
-  
 
-  it('valid log in with system_admin credentials', () => {
+  it('valid login with system_admin credentials', () => {
     cy.intercept('POST', '**/auth/login').as('loginRequest')
 
-    cy.get('input[placeholder="abc@gmail.com"]').type('yuvrajbisht41@gmail.com')
-    cy.get('input[placeholder="password"]').type('Yuvraj@2411')
-
-    cy.contains('Sign In').click()
+    cy.get('[data-cy="email-input"]').type('yuvrajbisht41@gmail.com')
+    cy.get('[data-cy="password-input"]').type('Yuvraj@2411')
+    cy.get('[data-cy="login-btn"]').click()
 
     cy.wait('@loginRequest')
       .its('response.statusCode')
       .should('eq', 200)
 
-    cy.url({ timeout: 15000 }).should('include', '/admin/categories')
+
+    cy.url().should('include', '/admin/categories')
 
     cy.window().then(win => {
       expect(win.localStorage.getItem('auth_token')).to.exist
     })
   })
+
 
 
   it('blocks unauthenticated user from admin routes', () => {
