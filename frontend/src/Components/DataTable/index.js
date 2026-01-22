c80f3a4e2773e5a7e80ef81520e3b5e3f78fed5c
yuvraj249 <yuvraj@tech-x.com>
2026-01-22 09:49:36 +0530

    modified data-cy attribute in datatable file

--- frontend/src/Components/DataTable/index.js
+++ frontend/src/Components/DataTable/index.js
@@ -24,7 +24,7 @@ const Datatable = (props = initialProps) => {
 
 
   return (
-    <TableWrapper data-cy="suppliers-table">
+    <TableWrapper data-cy="data-table">
         <Table>
             <thead>
                 {table.getHeaderGroups().map((hg) => (
