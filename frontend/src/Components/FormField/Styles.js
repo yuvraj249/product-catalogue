17c61e348db7b4529dfd6a164379aeba5eb4a4d5
yuvraj249 <yuvraj@tech-x.com>
2026-01-30 11:18:26 +0530

    added common component for form fields

---
+++ frontend/src/Components/FormField/Styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  margin-bottom: 16px;
+`;
+
+export const Label = styled.label`
+  font-size: 14px;
+  font-weight: 600;
+  color: #1f2d36;
+`;
